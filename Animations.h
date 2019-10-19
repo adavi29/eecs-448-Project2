09c3c27637658ba975720b7eeb870d8e06e08e63
Abigail Davidow <lenexapaws@gmail.com>
2019-10-19 17:15:17 -0500

    Added animation documentation

--- Animations.h
+++ Animations.h
@@ -14,13 +14,43 @@ using namespace std;
 class Animations{
 
  public:
+    /*
+     *  @post creates Animations object
+     */
    Animations();
+    /*
+    *  @post deletes Animations object
+    */
    ~Animations();
+    /*
+    *  @pre player must have hit part of a ship
+    *  @post plays hit animation
+    */
    void playHit();
+    /*
+    *  @pre player must have missed their shot
+    *  @post plays miss animation
+    */
    void playMiss();
+    /*
+    *  @pre player must have hit all parts of the ship
+    *  @post plays sunk ship animation
+    */
    void playSunk();
+    /*
+    *  @pre player 1 has sunk all of player 2's ships
+    *  @post plays player 1 wins animation
+    */
    void playP1Wins();
+    /*
+    *  @pre player 2 has sunk all of player 1's ships
+    *  @post plays player 2 wins animation
+    */
    void playP2Wins();
+    /*
+    *  @pre AI has hit all of the player's ships
+    *  @post plays AI wins animation
+    */
    void playAIWins();
 };
 #endif
